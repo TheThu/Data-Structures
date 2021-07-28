c74280813851fe3802b1b82195d1b4b0aa2f88c0
TheThu <thethu.tran@gmx.net>
2021-07-29 00:01:09 +0200

    Create Linked List Node

---
+++ Linked_List.h
@@ -0,0 +1,17 @@
+/*
+ * Linked_List.h
+ *
+ *  Created on: 14 Jul 2021
+ *      Author: T2
+ */
+
+#ifndef LINKED_LIST_H_
+#define LINKED_LIST_H_
+
+
+struct Node{
+    int data;
+    struct Node *next;
+ };
+
+#endif /* LINKED_LIST_H_ */
---
+++ main.c
@@ -0,0 +1,34 @@
+/*
+ * main.c
+ *
+ *  Created on: 14 Jul 2021
+ *      Author: T2
+ */
+
+
+#include "Linked_List.h"
+#include "stdio.h"
+
+
+
+int main()
+{
+	// Define pointer
+	struct Node* A;
+	// Empty List
+	A = NULL;
+
+	// allocating space in memory of size Node
+	struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+
+	temp->data= 2;
+	temp->next=NULL;
+
+	A=temp;
+
+	// Printing message
+	printf("-----");
+    printf("%d",A->data);
+    printf("-----");
+    return 0;
+}
