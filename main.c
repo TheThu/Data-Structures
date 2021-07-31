65804155093d4b13297d8a64b2f4e68e44e74818
TheThu <thethu.tran@gmx.net>
2021-07-31 09:41:32 +0200

    Toogle Comments

--- main.c
+++ main.c
@@ -14,22 +14,22 @@
 int main()
 {
 
-	// Create empty list
-	Node* A = NULL;
-
-	// Allocating memory block of size Node during run time
-	Node * temp= (Node*)malloc(sizeof(A));
-
-	// Filling List with value
-	temp->data = 1337;
-
-	// Pointer to next to NULL
-	temp->next = NULL;
-
-	// Writing adress in A
-	A = temp;
-
-	// Printing Node data
-	printf("Value is %d \n", A->data);
+//	// Create empty list
+//	Node* A = NULL;
+//
+//	// Allocating memory block of size Node during run time
+//	Node * temp= (Node*)malloc(sizeof(A));
+//
+//	// Filling List with value
+//	temp->data = 1337;
+//
+//	// Pointer to next to NULL
+//	temp->next = NULL;
+//
+//	// Writing adress in A
+//	A = temp;
+//
+//	// Printing Node data
+//	printf("Value is %d \n", A->data);
     return 0;
 }
