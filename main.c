aa9b424defc41bc97e29cef020ee5a25852f2aed
TheThu <thethu.tran@gmx.net>
2021-07-31 09:28:02 +0200

    Insert Node

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main()
 	Node* A = NULL;
 
 	// Allocating memory block of size Node during run time
-	Node * temp= (Node*)malloc(sizeof(A));
+	Node * temp= (Node*)malloc(sizeof(Node));
 
 	// Filling List with value
 	temp->data = 1337;
@@ -26,10 +26,27 @@ int main()
 	// Pointer to next to NULL
 	temp->next = NULL;
 
+	// Insert Node
+
 	// Writing adress in A
 	A = temp;
 
+	temp = (Node*)malloc(sizeof(Node));
+
+	temp->data = 2;
+	temp->next = NULL;
+
+
+	Node* temp1 = A;
+	// Traverse list
+	while(temp1->next!=NULL)
+	{
+		temp1 = temp1->next;
+	}
+
+	temp1->next = temp;
+
 	// Printing Node data
-	printf("Value is %d \n", A->data);
+	printf("Value is %d \n", A->next->data);
     return 0;
 }
